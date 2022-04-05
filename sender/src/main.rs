5888d7aea25602bb895e0d39d6adf6c11460b3ed
Michael <mmm323790@gmail.com>
2022-04-05 19:34:04 +0800

    first commit

---
+++ sender/src/main.rs
@@ -0,0 +1,57 @@
+use std::net::{UdpSocket};
+use std::env;
+use std::io;
+
+struct Message {
+    id: u32,
+    name: String,
+    sample1: f64,
+}
+
+
+fn listen(socket: &UdpSocket, mut buf: &mut [u8]) -> usize {
+    let (num_of_bytes, src_ip) = socket.recv_from(&mut buf).expect("not receiving data");
+    println!("recv {} bytes from {}", num_of_bytes, src_ip);
+    num_of_bytes
+}
+
+fn send(socket: &UdpSocket, msg: &[u8], sender: &str) -> usize {
+    let result = socket.send_to(&msg, sender).expect("sending data error");
+    println!("sending {} bytes to {}", result, sender);
+    result
+}
+
+fn init(sender_port: &str, receiver_port: &str) -> (String, String, UdpSocket) {
+    let mut sender_ipaddr = String::from("127.0.0.1:");
+    let mut receiver_ipaddr = String::from("127.0.0.1:");
+    sender_ipaddr.push_str(sender_port);
+    receiver_ipaddr.push_str(receiver_port);
+    println!("sender: {}, receiver: {}", sender_ipaddr, receiver_ipaddr);
+
+    let socket = UdpSocket::bind(&sender_ipaddr).expect("couldn't bind to address");
+
+    let result = (sender_ipaddr, receiver_ipaddr, socket);
+    result
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let sender_port = &args[1];
+    let receiver_port = &args[2]; 
+    let (sender, receiver, socket) = init(sender_port, receiver_port);
+
+    let mut buf = [0; 100];
+
+    println!("start receiving");
+    loop {
+        let mut msg = String::new();
+        println!("please enter sending message.");
+        io::stdin().read_line(&mut msg).expect("read line error!");
+        let msg = msg.as_bytes();
+        send(&socket, &msg, &receiver);
+
+        listen(&socket, &mut buf);
+        println!("recv feedback OK.");
+    }
+    
+}
